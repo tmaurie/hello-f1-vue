22e2823acfd2ffda3d6e21bbed5f41182589cead
tmaurie <thomas.maurier91@gmail.com>
2021-12-12 01:23:37 +0100

    extract result component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -44,57 +44,7 @@
           </vac>
         </v-row>
       </v-card>
-      <v-card class="mt-8" v-if="!loading">
-        <v-row justify="center">
-          <h1 id="">Last Race Results</h1>
-        </v-row>
-
-        <v-card-title>
-
-          <v-img max-width="90" :src="getImgUrl(lastRace.Circuit.circuitId)"/>
-          <h3> {{ lastRace.raceName }}</h3> &nbsp;| {{ lastRace.Circuit.circuitName }}
-
-
-        </v-card-title>
-
-        <v-simple-table>
-          <template v-slot:default>
-            <thead>
-            <tr>
-              <th class="text-left">Pos.</th>
-              <th class="text-left">Driver</th>
-              <th class="text-left">Constructor</th>
-              <th class="text-left">Time</th>
-              <th class="text-left">Pts.</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr
-                v-for="(item,idx) in results"
-                :key="idx"
-            >
-              <td>{{ item.position }}</td>
-              <td v-if="$vuetify.breakpoint.xsOnly">{{ item.Driver.code  }}</td>
-              <td v-else>{{ item.Driver.familyName  }}</td>
-              <td>
-                <v-chip
-                    :color="getColor(item.Constructor.name)"
-                    dark
-                >
-                  {{ item.Constructor.name }}
-                </v-chip>
-              </td>
-              <td v-if="item.Time"><v-chip :color="item.FastestLap.rank === '1' ? 'purple' : ''">{{ item.Time.time }}</v-chip></td>
-              <td v-else>{{ item.status }}</td>
-              <td>{{ item.points }}</td>
-            </tr>
-
-            </tbody>
-          </template>
-
-        </v-simple-table>
-
-      </v-card>
+      <ResultCCard  :last-race="lastRace" :loading="loading" :results="results"/>
     </v-container>
   </v-img>
 
@@ -102,9 +52,11 @@
 
 <script>
 import axios from "axios";
+import ResultCCard from "@/pages/ResultCard";
 
 export default {
   name: "Home",
+  components: {ResultCCard},
   data() {
     return {
       loading: true,
@@ -156,34 +108,6 @@ export default {
 
           });
     },
-    getImgUrl(picture) {
-      return require('../assets/img/tracks/' + picture + '.png')
-    },
-    getColor(ecurie) {
-      switch (ecurie) {
-        case 'Red Bull' :
-          return 'indigo darken-2'
-        case 'Mercedes' :
-          return 'teal accent-3'
-        case 'Ferrari' :
-          return 'red'
-        case 'McLaren' :
-          return 'orange lighten-3'
-        case 'AlphaTauri' :
-          return 'light-blue darken-4'
-        case 'Alfa Romeo' :
-          return 'red  lighten-1'
-        case 'Haas F1 Team' :
-          return 'blue-grey lighten-1'
-        case 'Alpine F1 Team' :
-          return 'indigo darken-2'
-        case 'Williams' :
-          return 'blue accent-4'
-        case 'Aston Martin' :
-          return 'teal lighten-1'
-      }
-    }
-
   }
 
 }

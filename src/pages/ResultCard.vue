b8eb89ec5c7212df71cb66e3e6c0a5531553ba93
tmaurie <thomas.maurier91@gmail.com>
2022-01-27 13:56:30 +0100

    Extract color function

--- src/pages/ResultCard.vue
+++ src/pages/ResultCard.vue
@@ -54,6 +54,8 @@
   </v-card>
 </template>
 <script>
+import {getColor} from "@/plugins/services";
+
 export default {
   name: 'ResultCard',
   props: {
@@ -66,30 +68,7 @@ export default {
     getImgUrl(picture) {
       return require('../assets/img/tracks/' + picture + '.png')
     },
-    getColor(ecurie) {
-      switch (ecurie) {
-        case 'Red Bull' :
-          return 'indigo darken-2'
-        case 'Mercedes' :
-          return 'teal accent-3'
-        case 'Ferrari' :
-          return 'red'
-        case 'McLaren' :
-          return 'orange lighten-3'
-        case 'AlphaTauri' :
-          return 'light-blue darken-4'
-        case 'Alfa Romeo' :
-          return 'red  lighten-1'
-        case 'Haas F1 Team' :
-          return 'blue-grey lighten-1'
-        case 'Alpine F1 Team' :
-          return 'indigo darken-2'
-        case 'Williams' :
-          return 'blue accent-4'
-        case 'Aston Martin' :
-          return 'teal lighten-1'
-      }
-    }
+    getColor
   }
 }
 </script>
